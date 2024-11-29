6b59916106fb90180e6bae6467778e9f325f6805
trapperdot00 <csernusricsike@gmail.com>
2024-11-30 00:12:07 +0100

    added some voodoo magic to make the $ and $ command work. really messy, probably not efficient, but it works.

--- src/NewUserGen.cpp
+++ src/NewUserGen.cpp
@@ -38,6 +38,78 @@ NewUserGen::NewUserGen(std::ifstream &csvfile, std::ifstream &mapfile, Flags f)
 }
 
 std::ostream &NewUserGen::print(std::ostream &os) const {
+	// build map
+	std::map<std::string, std::string> header_to_param;
+	std::map<std::string, std::string> override_values;
+	std::map<std::string, std::string> fallback_values;
+	for (const MapRow &row : maprows) {
+		if (row.type() == MapRow::mapping) {
+			header_to_param.insert({row.right(), row.left()});
+		} else if (row.type() == MapRow::cmd_override) {
+			override_values.insert({row.parameter(), row.left().substr(find_nth(row.left(), 2, '$') + 1)});
+		} else if (row.type() == MapRow::cmd_fallback) {
+			fallback_values.insert({row.parameter(), row.left().substr(find_nth(row.left(), 2, '$') + 1)});
+		}
+	}
+
+	std::vector<std::vector<std::string>> command_lines;
+	for (std::size_t csvrow_i = 1; csvrow_i < csvrows.size(); ++csvrow_i) {
+		std::map<std::string, std::string> commands;
+		const CsvRow &header_row = csvrows[0];
+		const CsvRow &curr_csvrow = csvrows[csvrow_i];
+		for (std::vector<std::string>::size_type csvfield_i = 0; csvfield_i != csvrows[csvrow_i].size(); ++csvfield_i) {
+			const std::string &curr_csvfield = curr_csvrow[csvfield_i];
+			const std::string &curr_header_field = header_row[csvfield_i];
+			for (std::size_t maprow_i = 0; maprow_i != maprows.size(); ++maprow_i) {	
+				const MapRow &curr_maprow = maprows[maprow_i];
+				const std::string &parameter = curr_maprow.parameter();
+				const std::string &argument = curr_csvfield;
+				const ValueType &type = MapRow::parameter_value.find(parameter)->second;
+				std::map<std::string, std::string>::const_iterator m_it;
+				if (argument.empty()) break;
+				if (curr_maprow.type() == MapRow::mapping) {
+					if (curr_header_field == curr_maprow.left()) {
+						std::string formatted = format_argument(argument, type);
+						commands.insert({parameter, formatted});
+					}
+				} else { 
+					if ((m_it = header_to_param.find(parameter)) != header_to_param.cend() && m_it->second == curr_header_field) {
+						if (curr_maprow.type() == MapRow::cmd_override) {
+							if (override_values.find(parameter) != override_values.end())
+								commands[parameter] = format_argument(override_values.find(parameter)->second, type);
+						} else if (curr_maprow.type() == MapRow::cmd_fallback) {
+							if (fallback_values.find(parameter) != fallback_values.end())
+								commands.insert({parameter, format_argument(fallback_values.find(parameter)->second, type)});
+						}
+					}
+				}
+			}
+		}
+		
+		for (const auto &p : override_values) {
+			if (commands.find(p.first) == commands.cend())
+					commands.insert({p.first, format_argument(p.second, MapRow::parameter_value.find(p.first)->second)});
+		}
+		for (const auto &p : fallback_values) {
+			if (commands.find(p.first) == commands.cend())
+					commands.insert({p.first, format_argument(p.second, MapRow::parameter_value.find(p.first)->second)});
+		}
+
+		std::vector<std::string> user_commands;
+		for (const auto &p : commands) {
+			user_commands.push_back(p.first + ' ' + p.second);
+		}
+		command_lines.push_back(user_commands);
+	}
+	for (std::vector<std::string>::size_type i = 0; i != command_lines.size(); ++i) {
+		os << "New-ADUser";
+		for (std::vector<std::string>::size_type j = 0; j != command_lines[i].size(); ++j)
+			os << ' ' << command_lines[i][j];
+		if (i + 1 < command_lines.size())
+			os << '\n';
+	}
+
+	/*
 	for (std::size_t csvrow_i = 1; csvrow_i < csvrows.size(); ++csvrow_i) {
 		os << "New-ADUser";
 		const CsvRow &header_row = csvrows[0];
@@ -62,6 +134,7 @@ std::ostream &NewUserGen::print(std::ostream &os) const {
 		if (csvrow_i + 1 < csvrows.size())
 			os << '\n';
 	}
+	*/
 	return os;
 }
 
